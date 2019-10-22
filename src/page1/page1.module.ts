f1771d08de7f787c1a3001dd974393fe13b7c37f
John Doe <johndoe@example.com>
2019-10-22 23:01:42 +0100

    add page 2 pages

---
+++ src/page1/page1.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Page1RoutingModule } from './page1-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Page1RoutingModule
+  ],
+  declarations: []
+})
+export class Page1Module { }
---
+++ src/page2/page2.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { Page2RoutingModule } from './page2-routing.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    Page2RoutingModule
+  ],
+  declarations: []
+})
+export class Page2Module { }
